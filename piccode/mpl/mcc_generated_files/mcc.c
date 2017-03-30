bdfcd1092f5838bebff25c15f9019df49b60ed51
david.osorio <anorimaki@terra.com>
2017-03-30 06:36:51 +0200

    - Añadidos ficheros de configuración de los perifericos del PIC24 (MPLAB Code Configurator)

---
+++ piccode/mpl/mcc_generated_files/mcc.c
@@ -0,0 +1,96 @@
+/**
+  @Generated MPLAB(c) Code Configurator Source File
+
+  @Company:
+    Microchip Technology Inc.
+
+  @File Name:
+    mcc.c
+
+  @Summary:
+    This is the mcc.c file generated using MPLAB(c) Code Configurator
+
+  @Description:
+    This header file provides implementations for driver APIs for all modules selected in the GUI.
+    Generation Information :
+        Product Revision  :  MPLAB(c) Code Configurator - pic24-dspic-pic32mm : v1.25
+        Device            :  PIC24FJ32GA002
+        Driver Version    :  1.02
+    The generated drivers are tested against the following:
+        Compiler          :  XC16 1.26
+        MPLAB             :  MPLAB X 3.45
+*/
+
+/*
+    (c) 2016 Microchip Technology Inc. and its subsidiaries. You may use this
+    software and any derivatives exclusively with Microchip products.
+
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+    WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+    PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION
+    WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION.
+
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+    BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+    FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+    ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+    THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+
+    MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE
+    TERMS.
+*/
+
+// Configuration bits: selected in the GUI
+
+// CONFIG2
+#pragma config POSCMOD = NONE    // Primary Oscillator Select->Primary oscillator disabled
+#pragma config I2C1SEL = PRI    // I2C1 Pin Location Select->Use default SCL1/SDA1 pins
+#pragma config IOL1WAY = ON    // IOLOCK Protection->Once IOLOCK is set, cannot be changed
+#pragma config OSCIOFNC = ON    // Primary Oscillator Output Function->OSC2/CLKO/RC15 functions as port I/O (RC15)
+#pragma config FCKSM = CSDCMD    // Clock Switching and Monitor->Clock switching and Fail-Safe Clock Monitor are disabled
+#pragma config FNOSC = FRCPLL    // Oscillator Select->Fast RC Oscillator with PLL module (FRCPLL)
+#pragma config SOSCSEL = SOSC    // Sec Oscillator Select->Default Secondary Oscillator (SOSC)
+#pragma config WUTSEL = LEG    // Wake-up timer Select->Legacy Wake-up Timer
+#pragma config IESO = ON    // Internal External Switch Over Mode->IESO mode (Two-Speed Start-up) enabled
+
+// CONFIG1
+#pragma config WDTPS = PS32768    // Watchdog Timer Postscaler->1:32768
+#pragma config FWPSA = PR128    // WDT Prescaler->Prescaler ratio of 1:128
+#pragma config WINDIS = ON    // Watchdog Timer Window->Standard Watchdog Timer enabled,(Windowed-mode is disabled)
+#pragma config FWDTEN = OFF    // Watchdog Timer Enable->Watchdog Timer is disabled
+#pragma config ICS = PGx1    // Comm Channel Select->Emulator EMUC1/EMUD1 pins are shared with PGC1/PGD1
+#pragma config COE = OFF    // Set Clip On Emulation Mode->Reset Into Operational Mode
+#pragma config BKBUG = OFF    // Background Debug->Device resets into Operational mode
+#pragma config GWRP = OFF    // General Code Segment Write Protect->Writes to program memory are allowed
+#pragma config GCP = OFF    // General Code Segment Code Protect->Code protection is disabled
+#pragma config JTAGEN = OFF    // JTAG Port Enable->JTAG port is disabled
+
+#include "mcc.h"
+
+void SYSTEM_Initialize(void)
+{
+    PIN_MANAGER_Initialize();
+    INTERRUPT_Initialize();
+    OSCILLATOR_Initialize();
+    I2C2_Initialize();
+    UART1_Initialize();
+}
+
+void OSCILLATOR_Initialize(void)
+{
+    // NOSC FRCPLL; SOSCEN disabled; OSWEN Switch is Complete; 
+    __builtin_write_OSCCONL((uint8_t) (0x0100 & 0x00FF));
+    // RCDIV FRC/1; DOZE 1:8; DOZEN disabled; ROI disabled; 
+    CLKDIV = 0x3000;
+    // TUN Center frequency; 
+    OSCTUN = 0x0000;
+    // WDTO disabled; TRAPR disabled; SWDTEN disabled; EXTR disabled; POR disabled; SLEEP disabled; BOR disabled; IDLE disabled; IOPUWR disabled; VREGS disabled; CM disabled; SWR disabled; 
+    RCON = 0x0000;
+}
+
+/**
+ End of File
+*/
